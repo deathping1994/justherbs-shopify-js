871370b67d947b1fabb0bd35d0e265499bd9f25b
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-06-30 19:23:25 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,20 +1,38 @@
-setTimeout(function () {
-    $(".tdf_normal_btn").on("custom-event", function () {
+setTimeout(function() {
+    $(".tdf_normal_btn").on("custom-event", function() {
         console.log("custom");
-        $(".tdf_normal_btn").on("click touchstart", function (event) {
+        $(".tdf_normal_btn").on("click touchstart", function(event) {
             event.preventDefault();
             $(".tdf_normal_btn").click();
         });
     });
-    $(document).on(`load change`, '.tdf_input_discount', function () {
+    $(document).on(`load change`, '.tdf_input_discount', function() {
         console.log("started")
         var token = ""
-        $.getJSON('/cart.js', function (cart) { token = cart.token }); $(".tdf_normal_btn").on("click touchstart", function (event) {
-            event.preventDefault(); var basecode = $(".tdf_input_discount")[0].value; $.ajax({ type: "POST", url: "https://farzipromo-api-stage.farziengineer.co/discount", headers: { "Content-Type": "application/json" }, data: `{"code":"${basecode}", "cartId":"${token}"}`, }).then((response) => {
+        $.getJSON('/cart.js', function(cart) {
+            token = cart.token
+        });
+        $(".tdf_normal_btn").on("click touchstart", function(event) {
+            event.preventDefault();
+            var basecode = $(".tdf_input_discount")[0].value;
+            $.ajax({
+                type: "POST",
+                url: "https://farzipromo-api-stage.farziengineer.co/discount",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: `{"code":"${basecode}", "cartId":"${token}"}`,
+            }).then((response) => {
                 console.log("found")
-                if (response == "true" || response == "True") { $(".tdf_input_discount")[0].value = basecode; $(".tdf_normal_btn").click(); }
-            }).catch(() => { $(".tdf_input_discount")[0].value = basecode; $(".tdf_normal_btn").click(); });
-            
+                if (response == "true" || response == "True") {
+                    $(".tdf_input_discount")[0].value = basecode;
+                    $(".tdf_normal_btn").click();
+                }
+            }).fail(() => {
+                $(".tdf_input_discount")[0].value = basecode;
+                $(".tdf_normal_btn").click();
+            });
+
         });
     });
 }, 5000);
