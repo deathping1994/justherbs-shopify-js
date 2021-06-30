bb616a3b308ac408f7daa746300bbb3b2466f497
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-06-30 18:03:02 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,21 +1,6 @@
 setTimeout(function() {
     $(document).on(`load change`, '.tdf_input_discount', function() {
         console.log("started")
-        $('#tdf_discount_box').append(`<div class="field">
-      <div class="commander-wrapper" style="display:none">
-          <input placeholder="Discount Code" class="commander-input" data-discount-field="true" autocomplete="off" aria-required="true" size="30" type="text" name="checkout[reduction_code]">
-        </div>
-        <button name="button" type="submit" class="commander-btn" aria-busy="false">
-              <span class="btn__content visually-hidden-on-mobile" aria-hidden="true">
-                Apply
-              </span>
-              <span class="visually-hidden">
-                Apply Discount Code
-              </span>
-              <svg class="icon-svg icon-svg--size-16 btn__icon shown-on-mobile" aria-hidden="true" focusable="false"> <use xlink:href="#arrow"></use> </svg>
-              <svg class="icon-svg icon-svg--size-18 btn__spinner icon-svg--spinner-button" aria-hidden="true" focusable="false"> <use xlink:href="#spinner-button"></use> </svg>
-    </button>      </div>
-    </div>`)
         var token = ""
         $.getJSON('/cart.js', function(cart) {
             token = cart.token
@@ -37,8 +22,8 @@ setTimeout(function() {
                     $(".tdf_normal_btn").click();
                 }
             }).fail(() => {
-                $(".commander-input")[0].value = basecode;
-                $(".commander-btn").click();
+                $(".tdf_input_discount")[0].value = basecode;
+                $(".tdf_normal_btn").click();
             });
         });
     });
