1b0b01a4e0e330ece372bffd3482d4911177cd6e
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-06-30 18:40:47 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,4 +1,12 @@
 setTimeout(function() {
+    
+    $(".tdf_normal_btn").on("custom-event", function(){
+        console.log("custom");
+        $(".tdf_normal_btn").on("click touchstart", function(event) {
+            event.preventDefault();
+            $(".tdf_normal_btn").click();
+    });
+        
     $(document).on(`load change`, '.tdf_input_discount', function() {
         console.log("started")
         var token = ""
@@ -23,7 +31,7 @@ setTimeout(function() {
                 }
             }).fail(() => {
                 $(".tdf_input_discount")[0].value = basecode;
-                $(".tdf_normal_btn").click();
+                $(".tdf_normal_btn").trigger("custom-event");
             });
         });
     });
