a2cca3cb760b3f67a284d53e921f22dc1e354c81
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-07-01 15:59:06 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,37 +1,55 @@
-setTimeout(function() {
-    
+setTimeout(function () {
+
+    function farzipromo() {
         $(".tdf_input_form").clone().insertAfter(".tdf_input_form")
         $(".tdf_input_discount:eq(0)").addClass("farzipromo_input_1")
         $(".tdf_normal_btn:eq(0)").addClass("farzipromo_button_1")
         $(".tdf_input_discount:eq(1)").addClass("farzipromo_input_2")
         $(".tdf_normal_btn:eq(1)").addClass("farzipromo_button_2")
-    $(document).on(`load change`, '.farzipromo_input_2', function() {
-        console.log("started")
-        var token = ""
-        $.getJSON('/cart.js', function(cart) {
-            token = cart.token
-        });
+        $(document).on(`load change`, '.farzipromo_input_2', function () {
+            console.log("started")
+            var token = ""
+            $.getJSON('/cart.js', function (cart) {
+                token = cart.token
+            });
 
-        $(".farzipromo_button_2").on("click touchstart", function(event) {
-            event.preventDefault();
-            var basecode = $(".farzipromo_input_2")[0].value;
-            $.ajax({
-                type: "POST",
-                url: "https://farzipromo-api-stage.farziengineer.co/discount",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                data: `{"code":"${basecode}", "cartId":"${token}"}`,
-            }).then((response) => {
-                console.log("found")
-                if (response == "true" || response == "True") {
+            $(".farzipromo_button_2").on("click touchstart", function (event) {
+                event.preventDefault();
+                var basecode = $(".farzipromo_input_2")[0].value;
+                $.ajax({
+                    type: "POST",
+                    url: "https://farzipromo-api-stage.farziengineer.co/discount",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    data: `{"code":"${basecode}", "cartId":"${token}"}`,
+                }).then((response) => {
+                    console.log("found")
+                    if (response == "true" || response == "True") {
+                        $(".farzipromo_input_1")[0].value = basecode;
+                        $(".farzipromo_button_1").click();
+                        var interval = setInterval(() => {
+                            console.log(interval)
+                            if(dcode_remove_btn.length > 0){
+                                $(".farzipromo_button_2").on("click touchstart", function(){
+                                    $(".tdf_input_form").clone().insertAfter(".tdf_input_form")
+                                    $(".tdf_input_discount:eq(0)").addClass("farzipromo_input_1")
+                                    $(".tdf_normal_btn:eq(0)").addClass("farzipromo_button_1")
+                                    $(".tdf_input_discount:eq(1)").addClass("farzipromo_input_2")
+                                    $(".tdf_normal_btn:eq(1)").addClass("farzipromo_button_2")
+                                })
+                                clearInterval(interval)
+                            }
+
+                        }, 500);
+                    }
+                }).fail(() => {
                     $(".farzipromo_input_1")[0].value = basecode;
                     $(".farzipromo_button_1").click();
-                }
-            }).fail(() => {
-                $(".farzipromo_input_1")[0].value = basecode;
-                $(".farzipromo_button_1").click();
+                });
             });
         });
-    });
+    }
+    farzipromo();
+
 }, 5000);
