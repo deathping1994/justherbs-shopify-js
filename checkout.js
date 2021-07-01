5c90d02bd7126245b198fefb5065985c1f9e063a
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-07-01 15:40:56 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,16 +1,16 @@
 setTimeout(function() {
-
-    $(document).on(`load change`, '.tdf_input_discount', function() {
-        console.log("started")
-        var token = ""
-        $.getJSON('/cart.js', function(cart) {
-            token = cart.token
-        });
+    
         $(".tdf_input_form").clone().insertAfter(".tdf_input_form")
         $(".tdf_input_discount:eq(0)").addClass("farzipromo_input_1")
         $(".tdf_normal_btn:eq(0)").addClass("farzipromo_button_1")
         $(".tdf_input_discount:eq(1)").addClass("farzipromo_input_2")
         $(".tdf_normal_btn:eq(1)").addClass("farzipromo_button_2")
+    $(document).on(`load change`, '.farzipromo_input_2', function() {
+        console.log("started")
+        var token = ""
+        $.getJSON('/cart.js', function(cart) {
+            token = cart.token
+        });
 
         $(".farzipromo_button_2").on("click touchstart", function(event) {
             event.preventDefault();
