0132625dd8a43b85f8923ce2ed39106e4bea448c
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-07-06 13:02:31 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -123,10 +123,12 @@ setTimeout(function () {
 
             var interval = setInterval(() => {
                 if ($("#tdf_discount_box .tdf_input_form").length != 0 && $("#tdf_discount_box .tdf_discounted_dcode").length == 0) {
+                    console.log("show")
                     $("#tdf_discount_box_2").show()
                     $("#tdf_discount_box").hide()
                 }
                 if ($("#tdf_discount_box .tdf_discounted_dcode").length != 0) {
+                    console.log("hide")
                     $("#tdf_discount_box").show()
                     $("#tdf_discount_box_2").hide()
                 }
