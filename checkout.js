32c198a0888447d874089cbfd86adb362adcdb27
Gaurav Shukla <gshukla66@gmail.com>
2023-03-07 17:22:28 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,91 +1,44 @@
-setTimeout(function () {
-    var cartInterval = setInterval(function () {
-        if ($('#tdf_discount_box').length > 0 && $('#tdf_discount_box_2').length == 0 && $('#tdf_discount_box .tdf_input_form').length !=0 ) {
-            $('#tdf_discount_box').after(`<div id="tdf_discount_box_2" class="tdf_content_section_discounted tdf_container tdf_nonpopup">   <div class="tdf_input_form"><input type="text" class="tdf_input_discount" placeholder="Discount code"><button type="button" class="tdf_normal_btn" data-e="dcode_apply" tdfbinded="1">Apply</button></div> </div>`)
-            $('#tdf_discount_box').css({ "display": "none" })
-            $("#tdf_discount_box_2").css({ "text-align": "center", "margin-bottom": "10px" })
-            $("#tdf_discount_box_2 .tdf_input_form").css({ "color": "#212b36" })
-            $("#tdf_discount_box_2 .tdf_input_discount").css({
-                "width": "130px",
-                "border": "1px solid #d3d3db",
-                "margin-right": "5px",
-                "display": "inline-block",
-                "padding": "11px 12px",
-                "font-size": "14px",
-                "height": "36px",
-                "color": "#212b36",
-                "letter-spacing": ".8px",
-                "border-radius": "2px"
-            })
-            $("#tdf_discount_box_2 .tdf_normal_btn").css({
-                "background-color": "#5ea44c",
-                "border": "none",
-                "width": "80px",
-                "padding": "8px 13px",
-                "color": "white",
-                "line-height": "20px",
-                "border-radius": "2px",
-                "cursor": "pointer"
-            })
-            var token = ""
-            $.getJSON('/cart.js', function (cart) {
-                token = cart.token
-            });
-            $("#tdf_discount_box_2 .tdf_normal_btn").on("click touchstart", function (event) {
-                event.preventDefault();
-
-                var basecode = $("#tdf_discount_box_2 .tdf_input_discount")[0].value;
-                $.ajax({
-                    type: "POST",
-                    url: "https://justherbs-api.farziengineer.co/discount",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    data: `{"code":"${basecode}", "cartId":"${token}"}`,
-                }).then((response) => {
-                    if (response == "true" || response == "True") {
-                        $("#tdf_discount_box .tdf_input_discount")[0].value = basecode;
-                        $("#tdf_discount_box .tdf_normal_btn").click();
-                        setTimeout(function () {
-                            var couponlog_postrequest = {
-                                url: "https://justherbs-api.farziengineer.co/couponlog",
-                                method: "POST",
-                                timeout: 0,
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                            };
-                            var v = setInterval(function () {
-                                if ($(".tdf_notify").length != 0 && $(".tdf_notify").css("display") != "none") {
-                                    couponlog_postrequest.data = JSON.stringify({
-                                        coupon: basecode,
-                                        log: $(".tdf_notify div").text(),
-                                    });
-                                    $.ajax(couponlog_postrequest).done(function (response) {
-                                        console.log(response);
-                                    });
-                                    clearInterval(v);
-                                }
-                                else if ($("#tdf_discount_box .tdf_discounted_dcode").length.length != 0) {
-                                    couponlog_postrequest.data = JSON.stringify({
-                                        coupon: basecode,
-                                        log: $("#tdf_discount_box .tdf_discounted_dcode .tdf_coupon_mess").text(),
-                                    });
-                                    $.ajax(couponlog_postrequest).done(function (response) {
-                                        console.log(response);
-                                    });
-                                    clearInterval(v);
-                                }
-                            }, 500);
-                        }, 2000);
-
-                    }
-                }).fail(() => {
-                    $("#tdf_discount_box .tdf_input_discount")[0].value = basecode;
-                    $("#tdf_discount_box .tdf_normal_btn").click();
+$(document).on(`page:load page:change`, function () {
+    if ($('.order-summary__section--discount .fieldset').length > 0) {
+        $('.order-summary__section--discount .fieldset').append(`<div class="field">
+  <div class="commander-wrapper">
+      <input placeholder="Discount Code" class="commander-input" data-discount-field="true" autocomplete="off" aria-required="true" size="30" type="text" name="checkout[reduction_code]">
+      </div>
+      <button name="button" type="submit" class="commander-btn" aria-busy="false">
+          <span class="btn__content visually-hidden-on-mobile" aria-hidden="true">
+              Apply
+          </span>
+          <span class="visually-hidden">
+              Apply Discount Code
+          </span>
+          <svg class="icon-svg icon-svg--size-16 btn__icon shown-on-mobile" aria-hidden="true" focusable="false"> <use xlink:href="#arrow"></use> </svg>
+          <svg class="icon-svg icon-svg--size-18 btn__spinner icon-svg--spinner-button" aria-hidden="true" focusable="false"> <use xlink:href="#spinner-button"></use> </svg>
+  </button>      </div>
+  </div>`)
+        $('.commander-input')[0].style.display = "none";
+        $('.commander-btn')[0].style.display = "none";
+        var token = '';
+        $.get('/cart.js').then(response => {
+            token = $.parseJSON(response).token
+        }
+        )
+        $(".order-summary__section--discount .field__input-btn").on("click touchstart", function (event) {
+            event.preventDefault();
+            var basecode = $("#checkout_reduction_code")[0].value;
+            $.ajax({
+                type: "POST",
+                url: "https://justherbsfphapi.farziengineer.co/discount",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: `{"code":"${basecode}", "cartId":"${token}","app":"custom_app"}`,
+            }).then((response) => {
+                if (response == "true" || response == "True") {
+                    $(".commander-input")[0].value = basecode;
+                    $(".commander-btn").click();
                     setTimeout(function () {
                         var couponlog_postrequest = {
-                            url: "https://justherbs-api.farziengineer.co/couponlog",
+                            url: "https://justherbsfphapi.farziengineer.co/couponlog",
                             method: "POST",
                             timeout: 0,
                             headers: {
@@ -93,41 +46,189 @@ setTimeout(function () {
                             },
                         };
                         var v = setInterval(function () {
-                            if ($(".tdf_notify").length != 0 && $(".tdf_notify").css("display") != "none") {
+                            if ($(".edit_checkout .fieldset:last p").length != 0 && $(".edit_checkout .fieldset:last p").css("display") != "none") {
                                 couponlog_postrequest.data = JSON.stringify({
                                     coupon: basecode,
-                                    log: $(".tdf_notify div").text(),
+                                    log: $(".edit_checkout .fieldset:last p").text(),
+                                });
+                                $.ajax(couponlog_postrequest).done(function (response) {
+                                    console.log(response);
+                                });
+                                clearInterval(v);
+                            } else if ($(".notice.notice--warning .notice__content .notice__text").text().length > 0) {
+                                couponlog_postrequest.data = JSON.stringify({
+                                    coupon: basecode,
+                                    log: $(".notice.notice--warning .notice__content .notice__text").text(),
+                                });
+                                $.ajax(couponlog_postrequest).done(function (response) {
+                                    console.log(response);
+                                });
+                                clearInterval(v);
+                            } else if ($(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").length != 0) {
+                                couponlog_postrequest.data = JSON.stringify({
+                                    coupon: basecode,
+                                    log: $(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").text(),
                                 });
                                 $.ajax(couponlog_postrequest).done(function (response) {
                                     console.log(response);
                                 });
                                 clearInterval(v);
                             }
-                            else if ($("#tdf_discount_box .tdf_discounted_dcode").length.length != 0) {
+                        }, 1000);
+                    }, 3000);
+                }
+            }
+            ).catch(() => {
+                $(".commander-input")[0].value = basecode;
+                $(".commander-btn").click();
+                setTimeout(function () {
+                    var couponlog_postrequest = {
+                        url: "https://justherbsfphapi.farziengineer.co/couponlog",
+                        method: "POST",
+                        timeout: 0,
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    };
+                    var v = setInterval(function () {
+                        if ($(".edit_checkout .fieldset:last p").length != 0 && $(".edit_checkout .fieldset:last p").css("display") != "none") {
+                            couponlog_postrequest.data = JSON.stringify({
+                                coupon: basecode,
+                                log: $(".edit_checkout .fieldset:last p").text(),
+                            });
+                            $.ajax(couponlog_postrequest).done(function (response) {
+                                console.log(response);
+                            });
+                            clearInterval(v);
+                        } else if ($(".notice.notice--warning .notice__content .notice__text").text().length > 0) {
+                            couponlog_postrequest.data = JSON.stringify({
+                                coupon: basecode,
+                                log: $(".notice.notice--warning .notice__content .notice__text").text(),
+                            });
+                            $.ajax(couponlog_postrequest).done(function (response) {
+                                console.log(response);
+                            });
+                            clearInterval(v);
+                        } else if ($(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").length != 0) {
+                            couponlog_postrequest.data = JSON.stringify({
+                                coupon: basecode,
+                                log: $(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").text(),
+                            });
+                            $.ajax(couponlog_postrequest).done(function (response) {
+                                console.log(response);
+                            });
+                            clearInterval(v);
+                        }
+                    }, 1000);
+                }, 3000);
+            }
+            );
+        });
+        $(".edit_checkout .field__input-btn.btn").eq(0).on("click touchstart", function (event) {
+            event.preventDefault();
+            if ($("#checkout_reduction_code_mobile").length > 0) {
+                var basecode = $("#checkout_reduction_code_mobile")[0].value;
+                $.ajax({
+                    type: "POST",
+                    url: "https://justherbsfphapi.farziengineer.co/discount",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    data: `{"code":"${basecode}", "cartId":"${token}", "app":"custom_app"}`,
+                }).then((response) => {
+                    if (response == "true" || response == "True") {
+                        $(".commander-input")[0].value = basecode;
+                        $(".commander-btn").click();
+                        console.log("discount api");
+                        setTimeout(function () {
+                            var couponlog_postrequest = {
+                                url: "https://justherbsfphapi.farziengineer.co/couponlog",
+                                method: "POST",
+                                timeout: 0,
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                            };
+                            var v = setInterval(function () {
+                                if ($(".edit_checkout .fieldset:last p").length != 0 && $(".edit_checkout .fieldset:last p").css("display") != "none") {
+                                    couponlog_postrequest.data = JSON.stringify({
+                                        coupon: basecode,
+                                        log: $(".edit_checkout .fieldset:last p").text(),
+                                    });
+                                    $.ajax(couponlog_postrequest).done(function (response) {
+                                        console.log(response);
+                                    });
+                                    clearInterval(v);
+                                } else if ($(".notice.notice--warning .notice__content .notice__text").text().length > 0) {
+                                    couponlog_postrequest.data = JSON.stringify({
+                                        coupon: basecode,
+                                        log: $(".notice.notice--warning .notice__content .notice__text").text(),
+                                    });
+                                    $.ajax(couponlog_postrequest).done(function (response) {
+                                        console.log(response);
+                                    });
+                                    clearInterval(v);
+                                } else if ($(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").length != 0) {
+                                    couponlog_postrequest.data = JSON.stringify({
+                                        coupon: basecode,
+                                        log: $(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").text(),
+                                    });
+                                    $.ajax(couponlog_postrequest).done(function (response) {
+                                        console.log(response);
+                                    });
+                                    clearInterval(v);
+                                }
+                            }, 1000);
+                        }, 3000);
+                    }
+                }
+                ).catch(() => {
+                    $(".commander-input")[0].value = basecode;
+                    $(".commander-btn").click();
+                    console.log("discount api")
+                    setTimeout(function () {
+                        var couponlog_postrequest = {
+                            url: "https://justherbsfphapi.farziengineer.co/couponlog",
+                            method: "POST",
+                            timeout: 0,
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        };
+                        var v = setInterval(function () {
+                            if ($(".edit_checkout .fieldset:last p").length != 0 && $(".edit_checkout .fieldset:last p").css("display") != "none") {
                                 couponlog_postrequest.data = JSON.stringify({
                                     coupon: basecode,
-                                    log: $("#tdf_discount_box .tdf_discounted_dcode .tdf_coupon_mess").text(),
+                                    log: $(".edit_checkout .fieldset:last p").text(),
+                                });
+                                $.ajax(couponlog_postrequest).done(function (response) {
+                                    console.log(response);
+                                });
+                                clearInterval(v);
+                            } else if ($(".notice.notice--warning .notice__content .notice__text").text().length > 0) {
+                                couponlog_postrequest.data = JSON.stringify({
+                                    coupon: basecode,
+                                    log: $(".notice.notice--warning .notice__content .notice__text").text(),
+                                });
+                                $.ajax(couponlog_postrequest).done(function (response) {
+                                    console.log(response);
+                                });
+                                clearInterval(v);
+                            } else if ($(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").length != 0) {
+                                couponlog_postrequest.data = JSON.stringify({
+                                    coupon: basecode,
+                                    log: $(".tags-list .tag .tag__wrapper .reduction-code .reduction-code__text").text(),
                                 });
                                 $.ajax(couponlog_postrequest).done(function (response) {
                                     console.log(response);
                                 });
                                 clearInterval(v);
                             }
-                        }, 500);
-                    }, 2000);
-                });
-            });
-
-            var interval = setInterval(() => {
-                if ($("#tdf_discount_box .tdf_input_form").length != 0 && $("#tdf_discount_box .tdf_discounted_dcode").length == 0) {
-                    $("#tdf_discount_box_2").show()
-                    $("#tdf_discount_box").hide()
+                        }, 1000);
+                    }, 3000);
                 }
-                if ($("#tdf_discount_box .tdf_discounted_dcode").length != 0) {
-                    $("#tdf_discount_box").show()
-                    $("#tdf_discount_box_2").hide()
-                }
-            }, 200);
-        }
-    }, 1000);
-}, 5000);
+                );
+            }
+        });
+    }
+});
